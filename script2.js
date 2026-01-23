8e3369344457b5959ec5502bf81112d6c2479dd7
SutharshanMohanaruby <as20240600@sci.sjp.ac.lk>
2026-01-23 20:31:55 +0530

    DBMS Customer Registation

---
+++ script2.js
@@ -0,0 +1,18 @@
+const btn =document.querySelector(".register-btn");
+
+btn.addEventListener("click",function (){
+    const inputs = document.querySelectorAll("input");
+    let isEmpty = false;
+
+    inputs.forEach(input =>{
+        if(input.value===""){
+            isEmpty = true;
+        }
+    });
+
+    if(isEmpty) {
+        alert("Please fill all fields!");
+    }else{
+        alert("Registation succesful");
+    }
+});
